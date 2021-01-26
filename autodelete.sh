aaadc261a3e5e3ef4b5bf07464c6a66a49f54545
Andy Brown <andy@thebmwz3.co.uk>
2021-01-26 08:18:34 +0000

    Fixes for sorting

--- autodelete.sh
+++ autodelete.sh
@@ -35,7 +35,7 @@ if [ "$TYPE" == "mysql" ]; then
 	## MySQL processing
 	if [ "x$MYSQL_DB" == "x" ]; then
 		## DB is blank so use autodetection
-		MYSQL_DB=`mysqldump --skip-quote-names --skip-comments -N -c -A -n -t -d -u $MYSQL_USER --password=$MYSQL_PASS |grep MyVideos | awk '{print \$2;}' | cut -d ";" -f 1 | tail -n1`
+		MYSQL_DB=`mysqldump --skip-quote-names --skip-comments -N -c -A -n -t -d -u $MYSQL_USER --password=$MYSQL_PASS |grep MyVideos | awk '{print \$2;}' | cut -d ";" -f 1 | sort -V | tail -n1`
 	fi
 	echo "Mysql database: $MYSQL_DB"
 	LIST_FILES=`mysqldump --skip-quote-names --skip-comments --skip-extended-insert -N -c -n -u $MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB files --where="playCount>0" | cut -d "," -f 8,9 |grep "'"`
@@ -47,14 +47,14 @@ if [ "$TYPE" == "mysql" ]; then
 		if [ "x$FINDER" != "x" ]; then
 			echo "DELETE:  $FINDER"
 			if [ "$TESTING" == "0" ]; then
-				DORM=`rm -fv $FINDER`
+				DORM=`rm -fv "$FINDER"`
 				echo "RM output: $DORM"
 			fi
-			CHK_TVH=`grep $FINDER $PATH_TO_TVHEADEND_CONFIG/tvheadend/dvr/log/* | cut -d ":" -f 1`
+			CHK_TVH=`grep "$FINDER" $PATH_TO_TVHEADEND_CONFIG/tvheadend/dvr/log/* | cut -d ":" -f 1`
 			if [ "x$CHK_TVH" != "x" ]; then
 				echo "Find in TVH: $CHK_TVH"
 				if [ "$TESTING" == "0" ]; then
-					DORM=`rm -fv $CHK_TVH`
+					DORM=`rm -fv "$CHK_TVH"`
 					echo "RM output: $DORM"
 				fi
 			fi
@@ -72,17 +72,17 @@ else
         do
                 # echo "Entry: '$file_entry'"
 		CHK_FILE=`echo "$file_entry"`
-                FINDER=`find $PATH_TO_RECORDINGS -type f -mtime +$DAYS_TO_DELETE -name "$CHK_FILE" 2>/dev/null`
+                FINDER=`find "$PATH_TO_RECORDINGS" -type f -mtime +$DAYS_TO_DELETE -name "$CHK_FILE" 2>/dev/null`
                 if [ "x$FINDER" != "x" ]; then
                         echo "DELETE:  $FINDER"
 			if [ "$TESTING" -eq 0 ]; then
-                                DORM=`rm -f '$FINDER'`
+                                DORM=`rm -f "$FINDER"`
                         fi
-                        CHK_TVH=`grep $FINDER $PATH_TO_TVHEADEND_CONFIG/tvheadend/dvr/log/* | cut -d ":" -f 1`
+                        CHK_TVH=`grep "$FINDER" $PATH_TO_TVHEADEND_CONFIG/tvheadend/dvr/log/* | cut -d ":" -f 1`
                         if [ "x$CHK_TVH" != "x" ]; then
                                 echo "Find in TVH: $CHK_TVH"
 				if [ "$TESTING" -eq 0 ]; then
-                                        DORM=`rm -f '$CHK_TVH'`
+                                        DORM=`rm -f "$CHK_TVH"`
                                 fi
                         fi
                 fi
